329b612db28e2c643bb2ead7f21f06d9773c9a0b
Oluwatobi Daniels <towbeedaniels@gmail.com>
2020-07-13 16:33:10 +0100

    deleted html files

--- resources/views/users/inc/scripts.blade.php
+++
@@ -1,5 +0,0 @@
-<script src="{{asset('assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
-<script src="{{asset('assets/js/popper.min.js')}}"></script>
-<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
-<script src="{{asset('assets/js/plugins.js')}}"></script>
-<script src="{{asset('assets/js/main.js')}}"></script>
